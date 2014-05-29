219881f77fc44c637bb15d5a0cfd818c312c74b0
Ryan Foley <rtfoley@gmail.com>
2014-05-29 00:51:39 +0000

    Added additional validation to games form to ensure that winning score is at least 11, and overtime scores are separated by 2 points

--- app/collections/models.js
+++ app/collections/models.js
@@ -47,14 +47,24 @@ MatchFormSchema = new SimpleSchema({
 
 MatchFormSchema.messages({
   "samePlayer": "Players can not be the same",
-  "illegalWin": "Winner must win by at least 2 points",
-  "sameScore": "Game cannot end in a tie"
+  "winBy2": "Winner must win by at least 2 points",
+  "sameScore": "Game cannot end in a tie",
+  "playTo11": "Winner must have at least 11 points",
+  "illegalOvertime": "Winner can't win by more than 2 points if opponent has at least 10 points"
 });
 
 var checkScore = function(thisScore, theirScore) {
-  if(thisScore > theirScore && thisScore < theirScore + 2) {
-    return "illegalWin";
-  } else if(thisScore == theirScore) {
+  if(thisScore == theirScore) {
+    // no ties
     return "sameScore";
-  }
+  } else if(thisScore > theirScore && thisScore < 11) {
+    // winner must be 11 or greater
+    return "playTo11";
+  } else if(thisScore > theirScore && thisScore < theirScore + 2) {
+    // must win by 2
+    return "winBy2";
+  } else if(thisScore > theirScore && theirScore >= 10 && thisScore - theirScore != 2) {
+    // if the losing score is 10 or higher, winning score can only be 2 points higher
+    return "illegalOvertime";
+  } 
 }
